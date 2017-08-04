3bd92efba35c6190ceecb4af53cb5a7fc29a428a
Bclair <bclair@gmail.com>
2017-08-03 22:41:20 -0400

    Add files via upload
    Simple start to Battleship game

---
+++ battleship.js
@@ -0,0 +1,14 @@
+var location = 3;
+var location = 4;
+var location = 5;
+
+var guess;   // undefined until user makes a guess
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;  //will be true when ship is sunk
+
+while (isSunk == false) {
+    guess = prompt('Ready, Aim, Fire! (Enter a number from 0-6):');
+    
+}
