f640c08dfcc56ebde6a91c6ae39eeaf66e9f5e41
Bclair <bclair@gmail.com>
2017-08-03 22:47:57 -0400

    Add files via upload
    -stopped infinite loop
    -added guess tracking
    -added conditional statement for guesses between 0 - 6

--- battleship.js
+++ battleship.js
@@ -10,5 +10,9 @@ var isSunk = false;  //will be true when ship is sunk
 
 while (isSunk == false) {
     guess = prompt('Ready, Aim, Fire! (Enter a number from 0-6):');
-    
+    if (guess < 0 || guess > 6) {
+        alert('Please enter a valid cell number!');
+    } else {
+        guesses = guesses +1;
+    }
 }
