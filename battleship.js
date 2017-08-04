f92872f42c0a9b425fd91ccf463260ba23b7c2b0
Bclair <bclair@gmail.com>
2017-08-03 23:05:51 -0400

    fixed basic issues
    Fixed infinite loop
    added guess tracking
    basic game works now

--- battleship.js
+++ battleship.js
@@ -1,6 +1,6 @@
-var location = 3;
-var location = 4;
-var location = 5;
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
 
 var guess;   // undefined until user makes a guess
 var hits = 0;
@@ -13,6 +13,19 @@ while (isSunk == false) {
     if (guess < 0 || guess > 6) {
         alert('Please enter a valid cell number!');
     } else {
-        guesses = guesses +1;
+        guesses = guesses + 1;
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert('HIT!');
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert('You sank my battleship!');
+            }
+        }else{
+            alert('MISS!');
+            }
+        }
     }
-}
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+    "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
